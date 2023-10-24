74106db16df31975c77f8e83974f56d5f391f398
haoming <435203093@qq.com>
2023-10-24 09:35:20 +0800

    feat: update master

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -8,6 +8,9 @@ import {
 } from "./hightLight";
 import { parseFile } from "./parseVueSFC";
 import { debounce, setCursorPosition } from "./utils";
+
+const isDev = process.env.NODE_ENV !== "production";
+
 export class SidebarProvider implements vscode.WebviewViewProvider {
   _view?: vscode.WebviewView; //存储 Webview 视图
   private _context: vscode.ExtensionContext; // 存储扩展上下文对象
@@ -86,14 +89,12 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 
   //用于生成 Webview 的 HTML 内容
   private _getHtmlForWebview(webview: vscode.Webview) {
-    const isDev = process.env.NODE_ENV !== "production";
-
     const stylesUri = getUri(
       webview,
       this._extensionUri,
       isDev
         ? ["webview-ui", "build", "assets", "index.css"]
-        : ["assets", "index.css"]
+        : ["dist","assets", "index.css"]
     );
     // The JS file from the Vue build output
     const scriptUri = getUri(
@@ -101,7 +102,7 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
       this._extensionUri,
       isDev
         ? ["webview-ui", "build", "assets", "index.js"]
-        : ["assets", "index.js"]
+        : ["dist","assets", "index.js"]
     );
 
     const nonce = getNonce();
